aaa8ed68a68a128c14fa837ecaa9bc7e922442b5
Krishna Kumar <kkbhagat20@gmail.com>
2025-12-20 13:08:29 +0530

    set up fastify-socket.io and routes

--- app.js
+++ app.js
@@ -2,18 +2,53 @@ import "dotenv/config";
 import Fastify from "fastify";
 import connectDB from "./src/config/connect.js";
 import { PORT } from "./src/config/config.js";
+import fastifySocketIO from "fastify-socket.io";
+import { registerRoutes } from "./src/routes/index.js";
 
-const fastify = Fastify();
+const fastify = Fastify({ logger: true });
 
 const start = async () => {
-  await connectDB(process.env.MONGO_URI);
-  fastify.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(`Server is running on ${address}`);
-    }
-  });
+  try {
+    await connectDB(process.env.MONGO_URI);
+
+
+    fastify.register(fastifySocketIO, {
+      cors: {
+        origin: "*",
+      },
+      pingInterval: 10000,
+      pingTimeout: 5000,
+      transports: ["websocket"],
+    });
+
+    await registerRoutes(fastify);
+
+    fastify.ready().then(() => {
+      fastify.io.on("connection", (socket) => {
+        fastify.log.info("User connected:");
+
+        socket.on("joinRoom", (orderId) => {
+          socket.join(orderId);
+          fastify.log.info("User joined");
+        });
+
+        socket.on("disconnect", () => {
+          fastify.log.info("User disconnected");
+        });
+      });
+    });
+
+    fastify.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
+      if (err) {
+        fastify.log.error(err);
+      } else {
+        console.log(`Server is running on ${address}`);
+      }
+    });
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
 };
 
 start();
